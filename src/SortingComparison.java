4c0bc67b64bb9de55acbb25553777d80084613ec
matttse <tsemd@bu.edu>
2017-04-26 11:36:17 -0400

    using copies

--- src/SortingComparison.java
+++ src/SortingComparison.java
@@ -2,41 +2,48 @@
 public class SortingComparison {
 
 	public static void main(String[] args) {
+		int[ ] data = { 1000, 80, 10, 50, 70, 60, 90, 20, 30, 40, 0, -1000 };	
+		
+		int[ ] selectData = {0,0,0,0,0,0,0,0,0,0,0,0};
+		System.arraycopy(data, 0, selectData, 0, data.length);
 		long startTime = System.currentTimeMillis();
-		Select selection = new Select();
-		selection.main(args);
+		Select.selectionsort(selectData, 1, data.length-2);
 		long endTime = System.currentTimeMillis();
 		long elapsedTime = endTime - startTime;
 		System.out.print("Selection: ");
 		System.out.println(elapsedTime);		
-		
+
+		int[ ] insertData = {0,0,0,0,0,0,0,0,0,0,0,0};
+		System.arraycopy(data, 0, insertData, 0, data.length);
 		startTime = System.currentTimeMillis();
-		Insert insert = new Insert();
-		insert.main(args);
+		Insert.insertionsort(insertData, 1, insertData.length-2);
 		endTime = System.currentTimeMillis();
 		elapsedTime = endTime - startTime;
 		System.out.print("Insertion: ");
 		System.out.println(elapsedTime);
-		
+
+		int[ ] mergeData = {0,0,0,0,0,0,0,0,0,0,0,0};
+		System.arraycopy(data, 0, mergeData, 0, data.length);
 		startTime = System.currentTimeMillis();
-		Mergesort mergeSort = new Mergesort();
-		mergeSort.main(args);
+		Mergesort.mergesort(mergeData, 1, mergeData.length-2);
 		endTime = System.currentTimeMillis();
 		elapsedTime = endTime - startTime;
 		System.out.print("Merge: ");
 		System.out.println(elapsedTime);
-		
+
+		int[ ] quickData = {0,0,0,0,0,0,0,0,0,0,0,0};
+		System.arraycopy(data, 0, quickData, 0, data.length);
 		startTime = System.currentTimeMillis();
-		Quicksort quickSort = new Quicksort();
-		quickSort.main(args);
+		Quicksort.quicksort(quickData, 1, quickData.length-2);
 		endTime = System.currentTimeMillis();
 		elapsedTime = endTime - startTime;
 		System.out.print("Quicksort: ");
 		System.out.println(elapsedTime);
-		
+
+		int[ ] heapData = {0,0,0,0,0,0,0,0,0,0,0,0};
+		System.arraycopy(data, 0, heapData, 0, data.length);
 		startTime = System.currentTimeMillis();
-		Heapsort heapSort = new Heapsort();
-		heapSort.main(args);
+		Heapsort.heapsort(heapData, heapData.length);
 		endTime = System.currentTimeMillis();
 		elapsedTime = endTime - startTime;
 		System.out.print("Heapsort: ");
